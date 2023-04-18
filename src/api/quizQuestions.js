8f1372a3b937e9d69b27b463f6b771f51d10a44d
parth-verma7 <lcb2021028@iiitl.ac.in>
2023-04-19 01:28:51 +0530

    First Commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,56 @@
+var quizQuestions = [
+  {
+      question: "Do you like to play?",
+      answers: [
+          {
+              type: "True",
+              content: "True"
+          },
+          {
+              type: "False",
+              content: "False"
+          }
+      ]
+  },
+  {
+      question: "Do you like to sing?",
+      answers: [
+        {
+            type: "True",
+            content: "True"
+        },
+        {
+            type: "False",
+            content: "False"
+        }
+      ]
+  },
+  {
+      question: "Do you like to dance??",
+      answers: [
+        {
+            type: "True",
+            content: "True"
+        },
+        {
+            type: "False",
+            content: "False"
+        }
+      ]
+  },
+  {
+      question: "Which of these games do you think is best?",
+      answers: [
+        {
+            type: "True",
+            content: "True"
+        },
+        {
+            type: "False",
+            content: "False"
+        }
+      ]
+  }
+];
+
+export default quizQuestions;
